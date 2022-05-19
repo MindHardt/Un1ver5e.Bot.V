e1e000c6dbb036bb17625e525ecb923ec3efcfd4
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-05-20 00:35:02 +0500

    Started implementation of a Database

---
+++ Un1ver5e.Bot/Database/DatabaseController.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+
+namespace Un1ver5e.Bot.Database
+{
+    public class DatabaseController
+    {
+        private str
+        private NpgsqlConnection GetOpenedConnection()
+        {
+            var con = new NpgsqlConnection(connstr);
+            con.Open();
+            return con;
+        }
+
+
+
+    }
+}
