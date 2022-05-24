6725d9d22b70a95d5954d365ecf36ae6f499977b
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-05-25 02:52:39 +0500

    Added "challenge" command

--- Un1ver5e.Bot/Commands/BoardGamesCommands.cs
+++ Un1ver5e.Bot/Commands/BoardGamesCommands.cs
@@ -38,11 +38,48 @@ namespace Un1ver5e.Bot.Commands
         }
 
         [RequireGuild]
-        [Command("challenge"), Description("Решаем дела по-мужски.")]
-        public DiscordCommandResult Challenge(IMember mem)
+        [Command("challenge", "fight"), Description("Решаем дела по-мужски.")]
+        public DiscordCommandResult Challenge(IMember opponent)
         {
-            
-            return Reply();
+            IMember author = (IMember)Context.Author;
+
+            IThrowResult resultAuthor = service.ThrowByQuery("1d100");
+            IThrowResult resultOpponent = service.ThrowByQuery("1d100");
+
+            IMember? winner = Math.Sign(resultAuthor.GetCompleteSum().CompareTo(resultOpponent.GetCompleteSum())) switch
+            {
+                -1 => opponent,
+                0 => null,
+                1 => author,
+                _ => throw new Exception("What the fuck")
+            };
+
+            string title = winner is not null ?
+                $"Победил(а) {winner.Nick ?? winner.Name}!" :
+                "Ничья!";
+
+            LocalEmbed embed = new()
+            {
+                Fields = new LocalEmbedField[]
+                {
+                    new LocalEmbedField()
+                    {
+                        Name = author.Nick ?? author.Name,
+                        Value = resultAuthor.GetCompleteSum().ToString(),
+                        IsInline = true
+                    },
+                    new LocalEmbedField()
+                    {
+                        Name = opponent.Nick ?? opponent.Name,
+                        Value = resultOpponent.GetCompleteSum().ToString(),
+                        IsInline = true
+                    }
+                },
+                Title = title,
+                ThumbnailUrl = winner.GetGuildAvatarUrl()
+            };
+
+            return Reply(embed);
         }
     }
 }
